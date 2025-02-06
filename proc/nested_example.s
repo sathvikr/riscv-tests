4f8e4e08de7d860b1eea1789d51d5b099f072514
sathvikr <sathvik.redrouthu@gmail.com>
2025-02-06 01:00:16 -0800

    proc

---
+++ proc/nested_example.s
@@ -0,0 +1,16 @@
+/**
+* Recursive factorial function that computes n!
+*
+* C function:
+* long long int fact(long long int n) {
+*     if (n < 1) {
+*         return 1;
+*     } else {
+*         return n * fact(n - 1);
+*     }
+* }
+*
+* Register mapping:
+*   n: x10 - Input parameter n
+**/
+
